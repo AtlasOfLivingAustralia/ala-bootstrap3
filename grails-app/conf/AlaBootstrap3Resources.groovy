d84afb920bed63b2b84fcc346a98284c3c11e4af
djtfmartin <djtfmartin@gmail.com>
2015-02-11 20:25:58 +1100

    fixes to resource references

--- grails-app/conf/AlaBootstrap3Resources.groovy
+++ grails-app/conf/AlaBootstrap3Resources.groovy
@@ -1,20 +1,20 @@
 modules = {
     bootstrap {
         dependsOn 'core'
-        resource url:'/js/bootstrap.min.js', plugin:'ala-bootstrap3'
-        resource url:'/css/bootstrap.min.css', plugin:'ala-bootstrap3', attrs:[media:'screen, projection, print']
+        resource url: [ dir:'js', file: 'bootstrap.min.js', plugin:'ala-bootstrap3']
+        resource url: [ dir:'css', file: 'bootstrap.min.css', plugin:'ala-bootstrap3'], attrs:[media:'screen, projection, print']
     }
 
     ala {
         dependsOn 'bootstrap'
-        resource url:'/css/my-styles.css', plugin:'ala-bootstrap3', attrs:[media:'screen, projection, print']
+        resource url: [ dir:'css', file: 'my-styles.css', plugin:'ala-bootstrap3'], attrs:[media:'screen, projection, print']
         resource url:'http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css'
     }
 
     core {
         dependsOn 'jquery', 'autocomplete'
-        resource url: '/js/html5.js', plugin:'ala-bootstrap3', wrapper: { s -> "<!--[if lt IE 9]>$s<![endif]-->" }
-        resource url: '/js/application.js', plugin:'ala-bootstrap3', attrs:[media:'screen, projection, print']
+        resource url: [ dir:'js', file: 'html5.js', plugin:'ala-bootstrap3'], wrapper: { s -> "<!--[if lt IE 9]>$s<![endif]-->" }
+        resource url: [ dir:'js', file: 'application.js', plugin:'ala-bootstrap3'], attrs:[media:'screen, projection, print']
     }
 
     autocomplete {
@@ -22,13 +22,13 @@ modules = {
         dependsOn 'jquery-migration'
         // Important note!!: To use this component along side jQuery UI, you need to download a custom jQuery UI compilation that
         // do not include the autocommplete widget
-        resource url: '/css/jquery.autocomplete.css', plugin:'ala-bootstrap3', attrs:[media:'screen, projection, print']
-        resource url: '/js/jquery.autocomplete.js', plugin:'ala-bootstrap3', attrs:[media:'screen, projection, print']
+        resource url: [ dir:'css', file: 'jquery.autocomplete.css', plugin:'ala-bootstrap3'], attrs:[media:'screen, projection, print']
+        resource url: [ dir:'js', file: 'jquery.autocomplete.js', plugin:'ala-bootstrap3'], attrs:[media:'screen, projection, print']
     }
 
     'jquery-migration' {
         // Needed to support legacy js components that do not work with latest versions of jQuery
         dependsOn 'jquery'
-        resource url: '/js/jquery-migrate-1.2.1.min.js', plugin:'ala-bootstrap3'
+        resource url: [ dir:'js', file: 'jquery-migrate-1.2.1.min.js', plugin:'ala-bootstrap3']
     }
 }
