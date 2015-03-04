0f22a16e6beb2304ff94274c2b78aefd99657abf
Dave Martin <djtfmartin@gmail.com>
2015-03-04 15:20:13 +1100

    1.0 release

--- AlaBootstrap3GrailsPlugin.groovy
+++ AlaBootstrap3GrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class AlaBootstrap3GrailsPlugin {
     // the plugin version
-    def version = "1.0-SNAPSHOT"
+    def version = "1.0"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3 > *"
     // resources that are excluded from plugin packaging
