e4e85cccf33ea2f722cddd6325954f39f278e0e7
nickdos <nickdos@gmail.com>
2015-03-11 11:57:27 +1100

    Version 1.1-SNAPSHOT

--- AlaBootstrap3GrailsPlugin.groovy
+++ AlaBootstrap3GrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class AlaBootstrap3GrailsPlugin {
     // the plugin version
-    def version = "1.0"
+    def version = "1.1-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3 > *"
     // resources that are excluded from plugin packaging
