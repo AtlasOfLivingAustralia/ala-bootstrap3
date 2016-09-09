a7b5a3674b81a9a0c5ef7713771fdd5cb1a19e00
Nick dos Remedios <nickdos@gmail.com>
2016-09-09 16:50:48 +1000

    Removed deprecated 'projection' media type and removed media from JS declaration

--- grails-app/conf/AlaBootstrap3Resources.groovy
+++ grails-app/conf/AlaBootstrap3Resources.groovy
@@ -2,18 +2,18 @@ modules = {
     bootstrap {
         dependsOn 'core'
         resource url:grailsApplication.config.headerAndFooter.baseURL + '/js/bootstrap.min.js'
-        resource url: grailsApplication.config.headerAndFooter.baseURL + '/css/bootstrap.min.css', attrs:[media:'screen, projection, print']
+        resource url: grailsApplication.config.headerAndFooter.baseURL + '/css/bootstrap.min.css', attrs:[media:'screen, print']
     }
 
     ala {
         dependsOn 'bootstrap', 'font-awesome'
-        resource url: grailsApplication.config.headerAndFooter.baseURL + '/css/ala-styles.css', attrs:[media:'screen, projection, print']
+        resource url: grailsApplication.config.headerAndFooter.baseURL + '/css/ala-styles.css', attrs:[media:'screen, print']
     }
 
     core {
         dependsOn 'jquery', 'autocomplete'
         resource url: [ dir:'js', file: 'html5.js', plugin:'ala-bootstrap3'], wrapper: { s -> "<!--[if lt IE 9]>$s<![endif]-->" }
-        resource url: grailsApplication.config.headerAndFooter.baseURL + '/js/application.js', attrs:[media:'screen, projection, print']
+        resource url: grailsApplication.config.headerAndFooter.baseURL + '/js/application.js'
     }
 
     autocomplete {
@@ -21,8 +21,8 @@ modules = {
         dependsOn 'jquery-migration'
         // Important note!!: To use this component along side jQuery UI, you need to download a custom jQuery UI compilation that
         // do not include the autocomplete widget
-        resource url: [ dir:'css', file: 'jquery.autocomplete.css', plugin:'ala-bootstrap3'], attrs:[media:'screen, projection, print']
-        resource url: [ dir:'js', file: 'jquery.autocomplete.js', plugin:'ala-bootstrap3'], attrs:[media:'screen, projection, print']
+        resource url: [ dir:'css', file: 'jquery.autocomplete.css', plugin:'ala-bootstrap3'], attrs:[media:'screen, print']
+        resource url: [ dir:'js', file: 'jquery.autocomplete.js', plugin:'ala-bootstrap3'], attrs:[media:'screen, print']
     }
 
     'jquery-migration' {
