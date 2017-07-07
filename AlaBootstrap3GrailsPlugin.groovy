980ed79e240b7a3312723aa542b1ef99b6fe6fa3
Simon Bear <simon.bear@csiro.au>
2017-07-07 15:58:48 +1000

    Release 1.6.3

--- AlaBootstrap3GrailsPlugin.groovy
+++ AlaBootstrap3GrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class AlaBootstrap3GrailsPlugin {
     // the plugin version
-    def version = "1.6.3-SNAPSHOT"
+    def version = "1.6.3"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3 > *"
     // resources that are excluded from plugin packaging
