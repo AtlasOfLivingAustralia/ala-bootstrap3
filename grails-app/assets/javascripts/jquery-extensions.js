76d82809fc595df34bfe4edf15d66bab808678ec
Simon Bear <simon.bear@csiro.au>
2017-07-19 18:26:23 +1000

    Add require directive in jquery-extensions

--- grails-app/assets/javascripts/jquery-extensions.js
+++ grails-app/assets/javascripts/jquery-extensions.js
@@ -1,2 +1,2 @@
-//= jquery-migrate-1.2.1.min.js
-//= jquery.autocomplete-1.1/jquery.autocomplete.js
+//= require jquery-migrate-1.2.1.min.js
+//= require jquery.autocomplete-1.1/jquery.autocomplete.js
